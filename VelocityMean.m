b0b57911a6c4a786143a6f1c3ce6d61975a01162
Naomi <naomi.m.ellis97@gmail.com>
2023-10-20 12:16:50 -0600

    Create VelocityMean.m

---
+++ VelocityMean.m
@@ -0,0 +1,28 @@
+% Establish workspace
+clc;
+clear;
+close all;
+
+% Read in data
+inputFile = 'exampleFileName.csv';
+data = readmatrix(inputFile);
+
+% Clean data of nans
+data(isnan(data)) = [];
+
+% Implement scalingFactor
+% Placeholder: convertedData = data(appropriate columns)*scalingFactor
+
+% Need to move data into appropriate vectors for equation below
+
+% Calculate magnitude of difference in pressures
+magnitudeDeltaP = abs(P2-P1);
+
+% Calculate airspeed
+v=sqrt(2*magnitudeDeltaP.*((R*Tatm)./Patm));
+
+% Find max airspeed
+maxV = max(v);
+
+disp('Maximum Airspeed');
+disp(maxV);
