5f7e97cfa0a920fdd903f47f7af3a6bcb4b116b7
LucWillett04 <luc.willett@colorado.edu>
2023-10-27 13:16:50 -0600

    Create DescretizationFunction.m
    Discretization Function

---
+++ DescretizationFunction.m
@@ -0,0 +1,18 @@
+clc;clear;close all
+
+nSections = 4;
+Length = 10;
+w0 = 10;
+w1 = 4;
+
+dx = Length/nSections;
+x = 0:dx:Length;
+LineFunc = (((w1-w0)/Length).*x) + w0;
+
+Area = zeros(nSections,1);
+
+for i = 1:nSections
+Area(i) = ((LineFunc(i)-LineFunc(i+1))*dx)/2 + LineFunc(i+1)*dx;
+end
+
+sum(Area)
