930de74b1f4604d794d563ca63731ba97015d701
LucWillett <111764225+LucWillett04@users.noreply.github.com>
2023-10-28 14:50:39 -0600

    Update DescretizationFunction.m
    Now includes a working centroid function!

--- DescretizationFunction.m
+++ DescretizationFunction.m
@@ -3,7 +3,7 @@ clc;clear;close all
 nSections = 4;
 Length = 10;
 w0 = 10;
-w1 = 6;
+w1 = 0;
 
 dx = Length/nSections;
 x = 0:dx:Length;
@@ -20,8 +20,11 @@ end
 Centroid = zeros(nSections,1);
 
 for j = 1:nSections
-    Centroid(j) = (dx*(2*LineFunc(i) + LineFunc(i+1)))/(3*(LineFunc(i) + LineFunc(i+1)));
+    Centroid(j) = dx*(LineFunc(j) + 2*LineFunc(j+1))/(3*(LineFunc(j) + LineFunc(j+1)));
     if j > 1
         Centroid(j) = Centroid(j) + (dx*(j-1));
     end
 end
+
+disp(Area);
+disp(Centroid);
