1f9796ca93b88a6b7d8aa91016b6db20bee96be7
Sayer-Gage <me@sayergage.com>
2023-11-17 12:25:24 -0700

    New Integral functions for all three loading regimes

--- IntegralFunctions.m
+++ IntegralFunctions.m
@@ -8,73 +8,90 @@ I = ((0.5)*(0.12^3))/12;
 C = (1/(E*I));
 x = linspace(0,L,100);
 J = length(x);
-%z = linspace(zmin,zmax,100);
-%k = length(z);
 w1 = .95;
 w0 = 1.37;
 t = .12;
 
-[W] = load(J,x,w1,w0,L); %This is currently set for a trapezoidal load (Based on the example Cl graph)
-[V] = shear(W,x,L,J);
-[M] = bendingmoment(W,x,L,J);
-[vprime] = deflectionslope(W,x,L,J,C);
-[d] = deflection(W,x,L,J,C);
-maxdeflection = max(d);
-[S] = stress(W,L,I,t);
-[St] = strain(S,E);
+[Wrect,Wtri,Wtrap] = load(J,x,w1,w0,L); %This is currently set for a trapezoidal load (Based on the example Cl graph)
+[Vrect,Vtri,Vtrap] = shear(Wrect,Wtri,Wtrap,x,L,J);
+[Mrect,Mtri,Mtrap] = bendingmoment(Wrect,Wtri,Wtrap,x,L,J);
+[vprimerect,vprimetri,vprimetrap] = deflectionslope(Wrect,Wtri,Wtrap,x,L,J,C);
+[drect,dtri,dtrap] = deflection(Wrect,Wtri,Wtrap,x,L,J,C);
+rectmaxdeflection = max(drect);
+trimaxdeflection = max(dtri);
+trapmaxdeflection = max(dtrap);
+[Srect,Stri,Strap] = stress(Wrect,Wtri,Wtrap,L,I,t);
+[Strect, Sttri, Sttrap] = strain(Srect,Stri,Strap,E);
 
 
 
-%Shearforce = integral(load(x),0,L);
-%Bendingmoment = integral(shear(W,x,L),0,L);
-%Vslope = integral(bendingmoment(W,x,L),0,L);
-%deflection = integral(deflectionslope(W,x,L),0,L);
-
-
-
-function [W] = load(J,x,w1,w0,L)
-W = zeros(1,J);
+function [Wrect, Wtri, Wtrap] = load(J,x,w1,w0,L)
+Wtrap = zeros(1,J);
+Wtri = zeros(1,J);
+Wrect = zeros(1,J);
     for i = 1:J
-        W(i) = (((w1-w0)/L).*x(i)) + w0; %This will change with new function
+        Wtrap(i) = (((w1-w0)/L).*x(i)) + w0;
+        Wtri(i) = (w0/L).*x(i) + w0;
+        Wrect(i) = w0;
     end
 end
 
-function [V] = shear(W,x,L,J)
-V = zeros(1,J);
+function [Vrect,Vtri,Vtrap] = shear(Wrect,Wtri,Wtrap,x,L,J)
+Vrect = zeros(1,J);
+Vtri = zeros(1,J);
+Vtrap = zeros(1,J);
     for i = 1:J
-        V(i) = -(W(i).*x(i)) + (W(i).*L);
+        Vtrap(i) = -(Wtrap(i).*x(i)) + (Wtrap(i).*L);
+        Vtri(i) = -(Wtri(i).*x(i)) + (Wtri(i).*L);
+        Vrect(i) = -(Wrect(i).*x(i)) + (Wrect(i).*L);
     end
 end
 
-function [M] = bendingmoment(W,x,L,J)
-M = zeros(1,J);
+function [Mrect, Mtri, Mtrap] = bendingmoment(Wrect,Wtri,Wtrap,x,L,J)
+Mtrap = zeros(1,J);
+Mtri = zeros(1,J);
+Mrect = zeros(1,J);
     for i = 1:J
-     M(i) = (W(i).*x(i).^2)/2 - W(i).*L.*x(i) + (W(i).*L^2)/2;
+     Mtrap(i) = (Wtrap(i).*x(i).^2)/2 - Wtrap(i).*L.*x(i) + (Wtrap(i).*L^2)/2;
+     Mtri(i) = (Wtri(i).*x(i).^2)/2 - Wtri(i).*L.*x(i) + (Wtri(i).*L^2)/2;
+     Mrect(i) = (Wrect(i).*x(i).^2)/2 - Wrect(i).*L.*x(i) + (Wrect(i).*L^2)/2;
     end
 end
 
-function [vprime] = deflectionslope(W,x,L,J,C)
-vprime = zeros(1,J);
+function [vprimerect, vprimetri, vprimetrap] = deflectionslope(Wrect,Wtri,Wtrap,x,L,J,C)
+vprimerect = zeros(1,J);
+vprimetri = zeros(1,J);
+vprimetrap = zeros(1,J);
     for i = 1:J
-        vprime = (C)*(W(i).*x(i).^3)/6 - (W(i).*x(i).^2*L)/2 + (W(i).*L^2.*x(i)./2);
+        vprimetrap(i) = (C)*(Wtrap(i).*x(i).^3)/6 - (Wtrap(i).*x(i).^2*L)/2 + (Wtrap(i).*L^2.*x(i)./2);
+        vprimetri(i) = (C)*(Wtri(i).*x(i).^3)/6 - (Wtri(i).*x(i).^2*L)/2 + (Wtri(i).*L^2.*x(i)./2);
+        vprimerect(i) = (C)*(Wrect(i).*x(i).^3)/6 - (Wrect(i).*x(i).^2*L)/2 + (Wrect(i).*L^2.*x(i)./2);
     end
 end
 
-function [d] = deflection(W,x,L,J,C)
-d = zeros(1,J);
+function [drect,dtri,dtrap] = deflection(Wrect,Wtri,Wtrap,x,L,J,C)
+drect = zeros(1,J);
+dtri = zeros(1,J);
+dtrap = zeros(1,J);
 for i = 1:J
-    d(i) = (C)*(((W(i).*x(i).^4)./24) - ((W(i).*x(i).^3*L)./6) + ((W(i).*L^2.*x(i).^2)./4));
+    dtrap(i) = (C)*(((Wtrap(i).*x(i).^4)./24) - ((Wtrap(i).*x(i).^3*L)./6) + ((Wtrap(i).*L^2.*x(i).^2)./4));
+    dtri(i) = (C)*(((Wtri(i).*x(i).^4)./24) - ((Wtri(i).*x(i).^3*L)./6) + ((Wtri(i).*L^2.*x(i).^2)./4));
+    drect(i) = (C)*(((Wrect(i).*x(i).^4)./24) - ((Wrect(i).*x(i).^3*L)./6) + ((Wrect(i).*L^2.*x(i).^2)./4));
 end
 end
 
-function [S] = stress(W,L,I,t)
+function [Srect,Stri,Strap] = stress(Wrect,Wtri,Wtrap,L,I,t)
     %for i = 1:J
-        S = -((max(W).*L^2*(t/2))/(2*I));
+        Strap = -((max(Wtrap).*L^2*(t/2))/(2*I));
+        Stri = -((max(Wtri).*L^2*(t/2))/(2*I));
+        Srect = -((max(Wrect).*L^2*(t/2))/(2*I));
     %end
 end
 
-function [St] = strain(S,E)
+function [Strect,Sttri,Sttrap] = strain(Srect,Stri,Strap,E)
     %for i = 1:iterator
-         St = S/E;
+         Strect = Srect/E;
+         Sttri = Stri/E;
+         Sttrap = Strap/E;
     %end
 end
